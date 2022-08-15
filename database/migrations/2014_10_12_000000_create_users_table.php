8d680c7b80a453af151cefb14c0cb0a3f5f0abc5
phamthimaituyet <phamthimaituyet2000@gmail.com>
2022-08-15 08:21:14 +0700

    chapter2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('isAdmin')->default(0);
+            $table->boolean('isActive')->default(0);
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
     }
