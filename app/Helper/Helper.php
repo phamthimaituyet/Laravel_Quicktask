0f4eacbf81fe9a5a1557d9b5c02f2ddd50030e24
phamthimaituyet <phamthimaituyet2000@gmail.com>
2022-08-22 16:57:52 +0700

    chapter14

---
+++ app/Helper/Helper.php
@@ -0,0 +1,12 @@
+<?php 
+
+if (!function_exists('formatDate')) {
+    function formatDate($date, string $format = 'D/m/Y')
+    {
+        if ($date instanceof \Carbon\Carbon) {
+            return $date->format($format);
+        }
+
+        return $date;
+    }
+}
